543365052482d823ee7bd11eb9bca27b31369979
Hillary <hillarychang11@gmail.com>
2022-08-08 19:19:54 -0700

    changes

--- coding_dojo/flask_first/checker.py
+++ coding_dojo/flask_first/checker.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# @app.route('/play')
+# @app.route('/')
 # def index():
-#     return render_template("index.html")	
+#     return render_template("checker.html")	
 
-@app.route('/play/<int:x>/<int:y>')
+@app.route('/<int:x>/<int:y>')
 def change_color(x, y):
     return render_template("checker.html", x=x, y=y)	
 

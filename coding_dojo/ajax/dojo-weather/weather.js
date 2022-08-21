75d5eccc52c760d40b7594473b1c3815b78215c9
Hillary <hillarychang11@gmail.com>
2022-08-21 15:11:30 -0700

    changes

--- coding_dojo/ajax/dojo-weather/weather.js
+++ coding_dojo/ajax/dojo-weather/weather.js
@@ -1,13 +1,13 @@
 
 
 
-let getAPI = () => {
+let getWeather = () => {
     fetch("http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&APPID=97bd75684cc7cf194dc007851d337d78")
     // Make sure to put your unique API key in the URL (taking out the brackets).
     // &APPID={INSERTAPIKEY} will need to be at the end of each URL you access below and in the assignment.
     .then(response => response.json() )
     .then(coderData =>{ console.log(coderData) 
-        document.getElementById('obj').innerHTML = `<h1> login = ${coderData.} </h1>`
+        document.getElementById('obj').innerHTML = `<h1> login = ${coderData} </h1>`
         // document.getElementById('obj').innerHTML += `<img src = ${coderData.}>`
 
 }

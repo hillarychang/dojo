fb0513690b8f89bf1cdec4d4fbfa13871bf8f799
Hillary <hillarychang11@gmail.com>
2022-08-23 19:03:11 -0700

    push

---
+++ coding_dojo/2exam/flask_app/controllers/controller_trees.py
@@ -0,0 +1,126 @@
+from flask import render_template, redirect, request, session, flash
+
+from flask_app import app
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+from flask_app.models.tree import Tree
+from flask_app.models.user import User
+
+app.secret_key = "shhh"
+
+
+
+@app.route("/show_sighting_users/<int:id>")  #ID comes from -> check index.html route
+def show_sighting_users(id):
+    data = {"id":id}
+    one_sighting = Sighting.get_one(data)
+    current_user = User.get_one({'id':session['user_id']})
+
+    status = Sighting.checkStatus({"user_id":session['user_id'], "sighting_id":id})
+    print("status",status)
+
+
+
+    sighting_with_users = Sighting.get_sightings_with_skeptics(data) #[sighting.PY]gives one specific sighting
+        #returns a sighting with a list of users
+    # sighting.users
+    return render_template("view_sighting.html", curr_status = status, sighting_user = sighting_with_users, sighting = one_sighting, users = current_user)
+
+
+# @app.route("/show/<int:id>") #runs show-one-user page
+# def show_one(id):
+#     data = {'id':id}
+#     sightings = Sighting.get_one(data)
+
+#     user_data = {"id":session['user_id']} # need user's id
+#     user = User.get_one(user_data)
+
+#     another_user = User.get_one({'id':sightings.user_id})
+
+#     return render_template("view_sighting.html", one_sighting = sightings, users = user, other_user = another_user)
+
+
+
+
+@app.route("/sighting") #runs add recipe form
+def sighting():
+    
+    sightings = Sighting.get_all()
+    data = {"id":session['user_id']} # need user's id
+    # user = User.get_user_with_recipes(data) #returns a user with a list of posts
+
+    #ADDED
+    user = User.get_user_with_sightings(data) #returns a user with a list of recipes
+    return render_template("add_sighting.html", all_sightings = sightings, users = user) 
+
+
+
+
+
+
+#route to update
+@app.route("/update/<int:id>", methods=["POST"]) #deletes a user, doesn't run a page??
+def update_sighting(id):
+
+    if not Sighting.validate_sighting(request.form): #request.form  (check user.py)
+        return redirect('/update/<int:id>')
+
+    data = {
+        'id':id,
+        "location": request.form["location"],
+        "what_happened" : request.form["what_happened"],
+        "number" : request.form["number"],
+        "created_at" : request.form["created_at"]
+
+    }
+
+    Sighting.update(data)
+    return redirect('/showUser')
+    # return redirect(f'/show/{id}')
+
+
+@app.route("/edit/<int:id>") #update a user, runs edit page
+def edit_sighting(id):
+
+    data = {'id':id}
+    sightings = Sighting.get_one(data)
+    user = User.get_user_with_sightings({'id':sightings.user_id}) #returns a user with a list of recipes
+
+    return render_template("edit_sighting.html", sighting = sightings, users  = user)
+
+
+
+
+
+
+@app.route('/create_sighting', methods=["POST"])
+def create_recipe():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    
+    if not Sighting.validate_sighting(request.form): #request.form  (check user.py)
+        return redirect('/create_sighting') #???
+
+    
+    data = {
+
+            "location" : request.form["location"],
+            "what_happened" : request.form["what_happened"],
+            "number" : request.form["number"],
+            "user_id": session['user_id'],
+            "created_at": request.form["created_at"] #added this
+
+    }
+    # We pass the data dictionary into the save method from the Nina class.
+    id = Sighting.save(data)
+    return redirect('/showUser') 
+
+
+
+@app.route("/delete/<int:id>") #deletes a user, doesn't run a page??
+def delete_sighting(id):
+    data = {'id':id}
+    Sighting.delete(data)
+    return redirect('/showUser')
---
+++ coding_dojo/2exam/flask_app/controllers/controller_users.py
@@ -0,0 +1,130 @@
+from flask import render_template, redirect, request, session, flash
+
+from flask_app import app
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+from flask_app.models.user import User
+from flask_app.models.tree import Tree
+
+app.secret_key = "shhh"
+
+
+# controller_authors:add_favorite_author -> show_book_authors
+# show_book_authors
+
+
+
+@app.route("/delete_skeptic/<int:id>") #runs add recipe form
+def delete_skeptic(id):
+    data = {
+    "user_id":session['user_id'],
+    "sighting_id": id
+    } 
+
+    user = User.get_one({"id":session['user_id']})
+
+
+    sighting = Sighting.get_sightings_with_skeptics({'id':id}) #[sighting.PY]gives one specific sighting
+
+    User.delete_user_skeptics(data)  
+    return redirect(f'/show_sighting_users/{id}') #redirect goes to route, render_template shows html page
+
+
+
+
+@app.route("/create_skeptic/<int:id>") #runs add recipe form
+def create_skeptic(id):
+    data = {
+    "user_id":session['user_id'],
+    "sighting_id": id
+    } 
+
+    user = User.get_one({"id":session['user_id']})
+
+
+    sighting = Sighting.get_sightings_with_skeptics({'id':id}) #[sighting.PY]gives one specific sighting
+
+    User.add_to_user_skeptics(data)  
+            
+    #inset into skeptic
+    return redirect(f'/show_sighting_users/{id}') #redirect goes to route, render_template shows html page
+
+
+
+# REGISTRATION
+@app.route('/create_user', methods=['POST'])
+def create_user():
+
+    if not User.validate_user(request.form): #request.form  (check user.py)
+        return redirect('/')
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+
+    data = {
+        "first_name": request.form['fname'],
+        "last_name": request.form['lname'],
+        "email": request.form['email'],
+        "password" : pw_hash #assign hash to self.password
+    }
+
+    user_id = User.save(data)
+
+    print("ID",user_id)
+    # store user id into session
+    session['user_id'] = user_id
+    return redirect("/showUser")
+
+
+
+# LOGIN
+@app.route('/login', methods=['POST'])
+def login():
+
+    if not User.validate_login(request.form): #request.form  (check user.py)
+        return redirect('/')
+
+
+    data = { "email" : request.form["email"] }
+
+    user_in_db = User.get_by_email(data)
+
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+
+    session['user_id'] = user_in_db.id #create session with user_in_db.id 
+
+    return redirect("/showUser")
+
+
+@app.route("/showUser") #runs starting form
+def showUser():
+    
+    sightings = Sighting.get_all()
+    data = {"id":session['user_id']} # need user's id
+    user = User.get_user_with_sightings(data) #returns a user with a list of recipes
+    return render_template("result.html", all_sightings = sightings, users = user) 
+
+
+
+
+
+@app.route("/") #runs starting form
+def index():
+    return render_template("index.html") 
+
+
+
+@app.route("/log_out") 
+def log_out():
+    session.clear()
+    # session["counter"] = 1
+    return redirect('/')
+
+
